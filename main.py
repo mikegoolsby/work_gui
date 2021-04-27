9aff8c619f47e3cb589a97b76f6d0d43f12c67a4
mikegoolsby <55331311+mikegoolsby@users.noreply.github.com>
2021-04-27 16:45:08 -0400

    init main to repo

---
+++ main.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from datetime import date
+import random
+
+today = date.today()
+reps = ["Ryan F", "Max Goshen","Julian Nav","Ethan S", "Nick G", "Chris B","Aaron Jones","Connor McShane","Joe Onulak"]
+
+# Create a window object
+window = tk.Tk()
+window.title("ADSK TL")
+
+time = tk.Label(
+    text=f"Today's date is {today}",
+    foreground="#fa8d3e",
+    background="black",
+    height=8,
+    width=25
+    )
+
+
+def handle_click(event):
+    random_rep = random.sample(reps, 1)
+    print(random_rep)
+
+button = tk.Button(
+    text="Random rep",
+    foreground="white",
+    background="red",
+    width=25)
+
+button.bind("<Button-1>", handle_click)
+
+
+
+time.pack()
+button.pack()
+
+# Run the event loop
+window.mainloop()
